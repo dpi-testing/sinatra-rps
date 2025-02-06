0dcc96541940e03bff4e462f8ea89b1031da1fec
Chandhu Bhumireddy <cbhum@uic.edu>
2025-02-05 19:53:27 -0600

    new commit

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  erb(:rock)
+end
+
+get("/paper") do
+  erb(:paper)
+end
+
+get("/scissors") do
+  erb(:scissors)
 end
