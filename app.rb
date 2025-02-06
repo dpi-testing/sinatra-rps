c554c1f3d2bcb74419ec856c528407d7b3cc69cb
Chandhu Bhumireddy <cbhum@uic.edu>
2025-02-05 21:36:13 -0600

    new updates

--- app.rb
+++ app.rb
@@ -1,24 +1,54 @@
 require "sinatra"
 require "sinatra/reloader"
-require "better_errors"
-require "binding_of_caller"
-# Need this configuration for better_errors
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   erb(:home)
 end
 
 get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+
   erb(:rock)
 end
 
 get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else 
+    @outcome = "lost"
+  end
+
   erb(:paper)
 end
 
 get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
   erb(:scissors)
 end
